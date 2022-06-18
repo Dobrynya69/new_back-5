894f479ca01dc1d1b3cfa1854656415dd222a425
Roman <roma040805@gmail.com>
2022-06-18 18:40:09 +0300

    Adding forms in the app

--- BlogApp/tests.py
+++ BlogApp/tests.py
@@ -10,7 +10,8 @@ class BlogTests(TestCase):
 
     def test_post(self):
         post = self.post
-        self.assertEquals(str(post), post.title)
+        self.assertEqual(str(post), post.title)
+        self.assertEqual(post.get_absolute_url(), '/post/1/')
         self.assertEqual(post.author, self.user)
         self.assertEqual(post.title, 'title')
         self.assertEqual(post.text, 'body body')
@@ -18,16 +19,32 @@ class BlogTests(TestCase):
 
     def test_home_page(self):
         response = self.client.get(reverse('home'))
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'BlogApp/home.html')
         self.assertContains(response, 'title')
 
 
-    def test_post_page(self):
+    def test_detail_post(self):
         response = self.client.get('/post/1/')
-        wrong_response = self.client.get('post/wrong/')
-        self.assertEquals(response.status_code, 200)
-        self.assertEquals(wrong_response.status_code, 404)
+        wrong_response = self.client.get('/post/-1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(wrong_response.status_code, 404)
         self.assertTemplateUsed(response, 'BlogApp/post.html')
         self.assertContains(response, 'title')
 
+    
+    def test_create_new_post(self):
+        response = self.client.post(reverse('new_post'), {'author': self.user, 'title': 'testing title', 'text': 'testing text'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'testing title')
+
+
+    def test_update_post(self):
+        response = self.client.post(reverse('update_post', args='1'), {'title': 'update title', 'text': 'update text'})
+        print(response['location'])
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_delete_post(self):
+        response = self.client.post(reverse('delete_post', args='1'))
+        self.assertEqual(response.status_code, 302)
