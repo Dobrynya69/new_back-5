88403d8614cfce8720041f7b85471034ffaa065c
Roman <roma040805@gmail.com>
2022-06-18 22:22:48 +0300

    Prepare to heroku

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-fh&vtm2t-ptare56i0$!29$^9i1zqt$1cyl#w32j2y+^8ez$9_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
